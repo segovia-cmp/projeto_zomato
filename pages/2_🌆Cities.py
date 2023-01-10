b77f40ca5c30c8c400ce687ac7811f95bed545ea
Felipe Segovia <120282129+segovia-cmp@users.noreply.github.com>
2023-01-10 13:09:38 -0300

    modularizado

--- pages/2_🌆Cities.py
+++ pages/2_🌆Cities.py
@@ -74,39 +74,168 @@ COLORS = {
 def color_name(color_code):
   return COLORS[color_code]
 
+
+def clean_code(df):
+
+  """Função para limpeza do dataframe.
+  """
+   
+
+  #Limpar espaços em branco
+  df = df.drop_duplicates()
+
+  #Limpar 
+  df = df.dropna(axis = 0, how ='any').reset_index()
+
+
+  # Unifica os nomes dos tipos de cozinhas pelo primeiro nomes da lista
+  df["cuisines"] = df.loc[:, "cuisines"].apply(lambda x: x.split(",")[0])
+
+  df.drop(columns=['index'],inplace=True)
+
+
+  #Cria coluna country de acordo com os códigos fornecidos
+  df['country'] = df.loc[:, "country_code"].apply(lambda x: country_name(x))
+
+  #classifica o preço dos pratos por categoria
+  df["price"] = df.loc[:, "price_range"].apply(lambda x: create_price_tye(x))
+
+  #nomeia as cores de acordo com o nome
+  df["color"] = df.loc[:, "rating_color"].apply(lambda x: color_name(x))
+
+  #Unifica as diversas moedas em dolar.
+  df['price_in_dollar'] = df[['currency', 'average_cost_for_two']].apply( lambda x: ( x['average_cost_for_two'] / 12.85  ) if x['currency'] == 'Botswana Pula(P)'  else
+                                                                                        ( x['average_cost_for_two'] / 5.31  ) if x['currency'] == 'Brazilian Real(R$)' else
+                                                                                        ( x['average_cost_for_two'] / 1  ) if x['currency'] == 'Dollar($)' else
+                                                                                        ( x['average_cost_for_two'] / 3.67  ) if x['currency'] == 'Emirati Diram(AED)' else
+                                                                                        ( x['average_cost_for_two'] / 82.68  ) if x['currency'] == 'Indian Rupees(Rs.)' else
+                                                                                        ( x['average_cost_for_two'] / 15608.45  ) if x['currency'] == 'Indonesian Rupiah(IDR)' else
+                                                                                        ( x['average_cost_for_two'] / 1.57  ) if x['currency'] == 'NewZealand($)' else
+                                                                                        ( x['average_cost_for_two'] / 0.819257  ) if x['currency'] == 'Pounds(£)' else
+                                                                                        ( x['average_cost_for_two'] / 3.64  ) if x['currency'] == 'Qatari Rial(QR)' else
+                                                                                        ( x['average_cost_for_two'] / 17.59  ) if x['currency'] == 'Rand(R)' else
+                                                                                        ( x['average_cost_for_two'] / 366.86  ) if x['currency'] == 'Sri Lankan Rupee(LKR)' else
+                                                                                        ( x['average_cost_for_two'] / 18.65  ) if x['currency'] == 'Turkish Lira(TL)' else 0, axis=1 )
+  return df
+
+
+def top_restaurant_city (df):
+
+  """Função para ciração de gráfico com quantidaded e restaurantes por cidade.
+    Entrada: dataframe
+    Saída: fig (gráfico)
+  
+  """
+
+  aux = (df.loc[:,['city', 'restaurant_id','country']]
+          .groupby(['city', 'country'])
+          .count().sort_values('restaurant_id', ascending=False)
+          .reset_index()
+          .head(10))
+          
+  fig = px.bar(aux,x='city',y='restaurant_id'
+                  ,labels={'city': 'Cidades','restaurant_id': 'Quantidade de restaurantes','country': 'País'}
+                  ,color='country'
+                  ,template='plotly_dark'
+                  ,text='restaurant_id')
+
+  fig.update_layout(title_text='Top 10 com mais restaurates na base de dados', title_x=0.5)
+  fig.update_traces(textposition='auto',texttemplate='%{text:.2s}')
+  fig.update_yaxes(showticklabels=False)
+
+  return fig
+
+
+def rating_mean (df):
+            
+  aux_rating_mean = (df.loc[:, ['city','country','aggregate_rating', 'restaurant_id']]
+                                 .groupby(['country','city','restaurant_id'])
+                                 .mean()
+                                 .reset_index())
+
+
+  aux = (aux_rating_mean.loc[aux_rating_mean['aggregate_rating'] >= 4,:]
+                                  .sort_values('aggregate_rating', ascending=False)
+                                  .reset_index())
+
+  aux = (aux.loc[:, ['city','country', 'restaurant_id']]
+                      .groupby(['country','city'])
+                      .count()
+                      .sort_values('restaurant_id', ascending=False)
+                      .reset_index()
+                      .head(7))
+
+
+  fig = px.bar(aux,x='city',y='restaurant_id'
+                            ,labels={'city': 'Cidades','restaurant_id': 'Quantidade de restaurantes','country': 'País'}
+                            ,color='country'
+                            ,template='plotly_dark'
+                            ,text='restaurant_id')
+
+  fig.update_layout(title_text='Top 7 de restaurantes com média acima da 4', title_x=0.5)
+  fig.update_traces(textposition='auto',texttemplate='%{text:.2s}')
+  fig.update_yaxes(showticklabels=False)
+  return fig
+
+def rating_mean_down(df):
+
+  aux_rating_mean = (df.loc[:, ['city','country','aggregate_rating', 'restaurant_id']]
+                     .groupby(['country','city','restaurant_id'])
+                     .mean()
+                     .reset_index())
+
+
+  aux = (aux_rating_mean.loc[aux_rating_mean['aggregate_rating'] <= 2.5,:]
+                                  .sort_values('aggregate_rating', ascending=True)
+                                  .reset_index())
+
+  aux = (aux.loc[:, ['city','country', 'restaurant_id']]
+                      .groupby(['country','city'])
+                      .count()
+                      .sort_values('restaurant_id', ascending=False)
+                      .reset_index()
+                      .head(7))
+
+
+  fig = px.bar(aux,x='city',y='restaurant_id'
+                            ,labels={'city': 'Cidades','restaurant_id': 'Quantidade de restaurantes','city': 'Cidades'}
+                            ,color='country'
+                            ,template='plotly_dark'
+                            ,text='restaurant_id')
+
+  fig.update_layout(title_text='Top 7 com restaurante com média abaixo de 2.5', title_x=0.5)
+  fig.update_traces(textposition='auto',texttemplate='%{text:.2s}')
+  fig.update_yaxes(showticklabels=False)
+  return fig
+
+def distinctive_cuisine (df):  
+
+  aux_cuisines = (df.loc[:, ['city', 'cuisines','country']]
+                    .groupby(['country','city'])['cuisines']
+                    .nunique()
+                    .sort_values(ascending=False)
+                    .reset_index()
+                    .head(10))
+
+
+  fig = px.bar(aux_cuisines,x='city',y='cuisines'
+                          ,labels={'city': 'Cidades','cuisines': 'Quantidade de tipo de culinaria','Country': 'País'}
+                          ,color='country'
+                          ,template='plotly_dark'
+                          ,text='cuisines')
+
+  fig.update_layout(title_text='Top 10 de cidades com restaurantes com tipo culinários distintos', title_x=0.5)
+  fig.update_traces(textposition='auto',texttemplate='%{text:.2s}')
+  fig.update_yaxes(showticklabels=False)
+  return fig
+
 #==========================
 #Clear Dataframe
 #==========================
 
 df = rename_columns(df_raw)
 
-df = df.drop_duplicates()
-
-df = df.dropna(axis = 0, how ='any').reset_index()
-
-df["cuisines"] = df.loc[:, "cuisines"].apply(lambda x: x.split(",")[0])
-
-df.drop(columns=['index'],inplace=True)
-
-df['country'] = df.loc[:, "country_code"].apply(lambda x: country_name(x))
-
-df["price"] = df.loc[:, "price_range"].apply(lambda x: create_price_tye(x))
-
-df["color"] = df.loc[:, "rating_color"].apply(lambda x: color_name(x))
-
-df['price_in_dollar'] = df[['currency', 'average_cost_for_two']].apply( lambda x: ( x['average_cost_for_two'] / 12.85  ) if x['currency'] == 'Botswana Pula(P)'  else
-                                                                                      ( x['average_cost_for_two'] / 5.31  ) if x['currency'] == 'Brazilian Real(R$)' else
-                                                                                      ( x['average_cost_for_two'] / 1  ) if x['currency'] == 'Dollar($)' else
-                                                                                      ( x['average_cost_for_two'] / 3.67  ) if x['currency'] == 'Emirati Diram(AED)' else
-                                                                                      ( x['average_cost_for_two'] / 82.68  ) if x['currency'] == 'Indian Rupees(Rs.)' else
-                                                                                      ( x['average_cost_for_two'] / 15608.45  ) if x['currency'] == 'Indonesian Rupiah(IDR)' else
-                                                                                      ( x['average_cost_for_two'] / 1.57  ) if x['currency'] == 'NewZealand($)' else
-                                                                                      ( x['average_cost_for_two'] / 0.819257  ) if x['currency'] == 'Pounds(£)' else
-                                                                                      ( x['average_cost_for_two'] / 3.64  ) if x['currency'] == 'Qatari Rial(QR)' else
-                                                                                      ( x['average_cost_for_two'] / 17.59  ) if x['currency'] == 'Rand(R)' else
-                                                                                      ( x['average_cost_for_two'] / 366.86  ) if x['currency'] == 'Sri Lankan Rupee(LKR)' else
-                                                                                      ( x['average_cost_for_two'] / 18.65  ) if x['currency'] == 'Turkish Lira(TL)' else 0, axis=1 )
-
+df = clean_code(df)
 
 
 #======================
@@ -138,22 +267,10 @@ df = df.loc[linhas_selecionadas, :]
 
 st.title( ' 🌆 Visão Cidades')
 
-aux = (df.loc[:,['city', 'restaurant_id','country']]
-         .groupby(['city', 'country'])
-         .count().sort_values('restaurant_id', ascending=False)
-         .reset_index()
-         .head(10))
-         
-fig = px.bar(aux,x='city',y='restaurant_id'
-                ,labels={'city': 'Cidades','restaurant_id': 'Quantidade de restaurantes','country': 'País'}
-                ,color='country'
-                ,template='plotly_dark'
-                ,text='restaurant_id')
 
-fig.update_layout(title_text='Top 10 com mais restaurates na base de dados', title_x=0.5)
-fig.update_traces(textposition='auto',texttemplate='%{text:.2s}')
-fig.update_yaxes(showticklabels=False)
 
+
+fig = top_restaurant_city(df)
 st.plotly_chart(fig, use_container_width=True)
 
 with st.container():
@@ -161,88 +278,19 @@ with st.container():
         
         with col1:
             
-            
-            aux_rating_mean = (df.loc[:, ['city','country','aggregate_rating', 'restaurant_id']]
-                                 .groupby(['country','city','restaurant_id'])
-                                 .mean()
-                                 .reset_index())
-
-
-            aux = (aux_rating_mean.loc[aux_rating_mean['aggregate_rating'] >= 4,:]
-                                  .sort_values('aggregate_rating', ascending=False)
-                                  .reset_index())
-
-            aux = (aux.loc[:, ['city','country', 'restaurant_id']]
-                      .groupby(['country','city'])
-                      .count()
-                      .sort_values('restaurant_id', ascending=False)
-                      .reset_index()
-                      .head(7))
-
-
-            fig = px.bar(aux,x='city',y='restaurant_id'
-                            ,labels={'city': 'Cidades','restaurant_id': 'Quantidade de restaurantes','country': 'País'}
-                            ,color='country'
-                            ,template='plotly_dark'
-                            ,text='restaurant_id')
-
-            fig.update_layout(title_text='Top 7 de restaurantes com média acima da 4', title_x=0.5)
-            fig.update_traces(textposition='auto',texttemplate='%{text:.2s}')
-            fig.update_yaxes(showticklabels=False)
-            
+           
+            fig  = rating_mean(df)
             st.plotly_chart(fig, use_container_width=True)
 
         
         
         with col2:
                         
-            aux_rating_mean = (df.loc[:, ['city','country','aggregate_rating', 'restaurant_id']]
-                     .groupby(['country','city','restaurant_id'])
-                     .mean()
-                     .reset_index())
-
-
-            aux = (aux_rating_mean.loc[aux_rating_mean['aggregate_rating'] <= 2.5,:]
-                                  .sort_values('aggregate_rating', ascending=True)
-                                  .reset_index())
-
-            aux = (aux.loc[:, ['city','country', 'restaurant_id']]
-                      .groupby(['country','city'])
-                      .count()
-                      .sort_values('restaurant_id', ascending=False)
-                      .reset_index()
-                      .head(7))
-
-
-            fig = px.bar(aux,x='city',y='restaurant_id'
-                            ,labels={'city': 'Cidades','restaurant_id': 'Quantidade de restaurantes','city': 'Cidades'}
-                            ,color='country'
-                            ,template='plotly_dark'
-                            ,text='restaurant_id')
-
-            fig.update_layout(title_text='Top 7 com restaurante com média abaixo de 2.5', title_x=0.5)
-            fig.update_traces(textposition='auto',texttemplate='%{text:.2s}')
-            fig.update_yaxes(showticklabels=False)
-
+            
+            fig = rating_mean_down(df)
             st.plotly_chart(fig, use_container_width=True)
 
             
-aux_cuisines = (df.loc[:, ['city', 'cuisines','country']]
-                  .groupby(['country','city'])['cuisines']
-                  .nunique()
-                  .sort_values(ascending=False)
-                  .reset_index()
-                  .head(10))
 
-
-fig = px.bar(aux_cuisines,x='city',y='cuisines'
-                         ,labels={'city': 'Cidades','cuisines': 'Quantidade de tipo de culinaria','Country': 'País'}
-                         ,color='country'
-                         ,template='plotly_dark'
-                         ,text='cuisines')
-
-fig.update_layout(title_text='Top 10 de cidades com restaurantes com tipo culinários distintos', title_x=0.5)
-fig.update_traces(textposition='auto',texttemplate='%{text:.2s}')
-fig.update_yaxes(showticklabels=False)
-
+fig = distinctive_cuisine(df)
 st.plotly_chart(fig, use_container_width=True)

7a2e5cb61589d050c6a1cb6d7eb2e9c0d50f0a4c
Felipe Segovia <120282129+segovia-cmp@users.noreply.github.com>
2023-01-10 13:11:06 -0300

    Modularizado

--- pages/3_🍲Cuisines.py
+++ pages/3_🍲Cuisines.py
@@ -74,38 +74,76 @@ COLORS = {
 def color_name(color_code):
   return COLORS[color_code]
 
+
+def clean_code(df):
+
+  """Função para limpeza do dataframe.
+  """
+   
+
+  #Limpar espaços em branco
+  df = df.drop_duplicates()
+
+  #Limpar 
+  df = df.dropna(axis = 0, how ='any').reset_index()
+
+
+  # Unifica os nomes dos tipos de cozinhas pelo primeiro nomes da lista
+  df["cuisines"] = df.loc[:, "cuisines"].apply(lambda x: x.split(",")[0])
+
+  df.drop(columns=['index'],inplace=True)
+
+
+  #Cria coluna country de acordo com os códigos fornecidos
+  df['country'] = df.loc[:, "country_code"].apply(lambda x: country_name(x))
+
+  #classifica o preço dos pratos por categoria
+  df["price"] = df.loc[:, "price_range"].apply(lambda x: create_price_tye(x))
+
+  #nomeia as cores de acordo com o nome
+  df["color"] = df.loc[:, "rating_color"].apply(lambda x: color_name(x))
+
+  #Unifica as diversas moedas em dolar.
+  df['price_in_dollar'] = df[['currency', 'average_cost_for_two']].apply( lambda x: ( x['average_cost_for_two'] / 12.85  ) if x['currency'] == 'Botswana Pula(P)'  else
+                                                                                        ( x['average_cost_for_two'] / 5.31  ) if x['currency'] == 'Brazilian Real(R$)' else
+                                                                                        ( x['average_cost_for_two'] / 1  ) if x['currency'] == 'Dollar($)' else
+                                                                                        ( x['average_cost_for_two'] / 3.67  ) if x['currency'] == 'Emirati Diram(AED)' else
+                                                                                        ( x['average_cost_for_two'] / 82.68  ) if x['currency'] == 'Indian Rupees(Rs.)' else
+                                                                                        ( x['average_cost_for_two'] / 15608.45  ) if x['currency'] == 'Indonesian Rupiah(IDR)' else
+                                                                                        ( x['average_cost_for_two'] / 1.57  ) if x['currency'] == 'NewZealand($)' else
+                                                                                        ( x['average_cost_for_two'] / 0.819257  ) if x['currency'] == 'Pounds(£)' else
+                                                                                        ( x['average_cost_for_two'] / 3.64  ) if x['currency'] == 'Qatari Rial(QR)' else
+                                                                                        ( x['average_cost_for_two'] / 17.59  ) if x['currency'] == 'Rand(R)' else
+                                                                                        ( x['average_cost_for_two'] / 366.86  ) if x['currency'] == 'Sri Lankan Rupee(LKR)' else
+                                                                                        ( x['average_cost_for_two'] / 18.65  ) if x['currency'] == 'Turkish Lira(TL)' else 0, axis=1 )
+  return df
+
+def graf (df, condition):
+            aux = round(df.loc[:, ['cuisines','aggregate_rating']]
+              .groupby(['cuisines'])
+              .median()
+              .sort_values('aggregate_rating', ascending=condition)
+              .reset_index()
+              .head(num_slider),2)
+
+            fig = px.bar(aux,x='cuisines',y='aggregate_rating'
+                            ,labels={'cuisines': 'Tipo de culinária','aggregate_rating': 'Média da avaliação'}
+                            ,color_discrete_sequence=px.colors.qualitative.T10
+                            ,template='plotly_white'
+                            ,text='aggregate_rating')
+            fig.update_layout(title_text=(f'Top {num_slider} melhores tipos de culinária'), title_x=0.5)
+            fig.update_traces(textposition='inside',texttemplate='%{text:.2s}')
+            fig.update_yaxes(showticklabels=False)
+            
+            return fig
+
 #==========================
 #Clear Dataframe
 #==========================
 
 df = rename_columns(df_raw)
 
-df = df.drop_duplicates()
-
-df = df.dropna(axis = 0, how ='any').reset_index()
-
-df["cuisines"] = df.loc[:, "cuisines"].apply(lambda x: x.split(",")[0])
-
-df.drop(columns=['index'],inplace=True)
-
-df['country'] = df.loc[:, "country_code"].apply(lambda x: country_name(x))
-
-df["price"] = df.loc[:, "price_range"].apply(lambda x: create_price_tye(x))
-
-df["color"] = df.loc[:, "rating_color"].apply(lambda x: color_name(x))
-
-df['price_in_dollar'] = df[['currency', 'average_cost_for_two']].apply( lambda x: ( x['average_cost_for_two'] / 12.85  ) if x['currency'] == 'Botswana Pula(P)'  else
-                                                                                      ( x['average_cost_for_two'] / 5.31  ) if x['currency'] == 'Brazilian Real(R$)' else
-                                                                                      ( x['average_cost_for_two'] / 1  ) if x['currency'] == 'Dollar($)' else
-                                                                                      ( x['average_cost_for_two'] / 3.67  ) if x['currency'] == 'Emirati Diram(AED)' else
-                                                                                      ( x['average_cost_for_two'] / 82.68  ) if x['currency'] == 'Indian Rupees(Rs.)' else
-                                                                                      ( x['average_cost_for_two'] / 15608.45  ) if x['currency'] == 'Indonesian Rupiah(IDR)' else
-                                                                                      ( x['average_cost_for_two'] / 1.57  ) if x['currency'] == 'NewZealand($)' else
-                                                                                      ( x['average_cost_for_two'] / 0.819257  ) if x['currency'] == 'Pounds(£)' else
-                                                                                      ( x['average_cost_for_two'] / 3.64  ) if x['currency'] == 'Qatari Rial(QR)' else
-                                                                                      ( x['average_cost_for_two'] / 17.59  ) if x['currency'] == 'Rand(R)' else
-                                                                                      ( x['average_cost_for_two'] / 366.86  ) if x['currency'] == 'Sri Lankan Rupee(LKR)' else
-                                                                                      ( x['average_cost_for_two'] / 18.65  ) if x['currency'] == 'Turkish Lira(TL)' else 0, axis=1 )
+df = clean_code(df)
 
 
 
@@ -152,6 +190,7 @@ with st.container():
         
         with col1:
             
+          
             aux = round(df.loc[:, ['country', 'city','restaurant_name','cuisines','aggregate_rating']]
               .groupby(['country', 'city','restaurant_name','cuisines'])
               .mean()
@@ -163,6 +202,7 @@ with st.container():
             aux_n = aux.loc[0,'aggregate_rating']
 
             df_cuisine_1 = aux_n
+
             col1.metric ( aux_prt, df_cuisine_1 )
             
         with col2:
@@ -244,41 +284,13 @@ with st.container():
         
         with col1:
             
-            aux = round(df.loc[:, ['cuisines','aggregate_rating']]
-              .groupby(['cuisines'])
-              .median()
-              .sort_values('aggregate_rating', ascending=False)
-              .reset_index()
-              .head(num_slider),2)
+          fig = graf(df,False)
+            
+          st.plotly_chart(fig ,use_container_width=True)
 
-            fig = px.bar(aux,x='cuisines',y='aggregate_rating'
-                            ,labels={'cuisines': 'Tipo de culinária','aggregate_rating': 'Média da avaliação'}
-                            ,color_discrete_sequence=px.colors.qualitative.T10
-                            ,template='plotly_dark'
-                            ,text='aggregate_rating')
-            fig.update_layout(title_text=(f'Top {num_slider} melhores tipos de culinária'), title_x=0.5)
-            fig.update_traces(textposition='inside',texttemplate='%{text:.2s}')
-            fig.update_yaxes(showticklabels=False)
-            
-            st.plotly_chart(fig ,use_container_width=True)
-            
         with col2:
 
 
-            aux = round(df.loc[:, ['cuisines','aggregate_rating']]
-              .groupby(['cuisines'])
-              .median()
-              .sort_values('aggregate_rating', ascending=True)
-              .reset_index()
-              .head(num_slider),2)
-
-            fig = px.bar(aux,x='cuisines',y='aggregate_rating'
-                            ,labels={'cuisines': 'Tipo de culinária','aggregate_rating': 'Média da avaliação'}
-                            ,color_discrete_sequence=px.colors.qualitative.T10
-                            ,template='plotly_dark'
-                            ,text='aggregate_rating')
-            fig.update_layout(title_text=(f'Top {num_slider} piores tipos de culinária'), title_x=0.5)
-            fig.update_traces(textposition='inside',texttemplate='%{text:.2s}')
-            fig.update_yaxes(showticklabels=False)
+          fig = graf(df, True)
             
-            st.plotly_chart(fig ,use_container_width=True)
+          st.plotly_chart(fig ,use_container_width=True)
